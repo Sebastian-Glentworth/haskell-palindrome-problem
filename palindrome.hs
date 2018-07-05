e08d9f34d30b7f75ba018b12dfe13422d2226294
Sebastian Glentworth <seb@stian.co.uk>
2018-07-05 20:55:16 +0100

    initial commit

---
+++ palindrome.hs
@@ -0,0 +1,33 @@
+isPalindromicInt :: Int -> Bool
+isPalindromicInt n
+    | length(show n) == 1 = True
+    | isPalindromicString(show n) = True
+    | otherwise = False
+
+isPalindromicString :: String -> Bool
+isPalindromicString n
+    | n == [] = True
+    | length n == 1 = True
+    | head n /= last n = False
+    | isPalindromicString(tail(init n)) == True = True
+    | otherwise = False
+
+hasTwoFactorsOfLength :: Int -> Int -> Bool
+hasTwoFactorsOfLength x n
+    | x `isMultipleOf` factors = True
+    | otherwise = False
+    where factors = [xs | xs <- factorsOf x, xs < (10 ^ n) && xs > 10 ^ (n - 1)]
+
+isMultipleOf :: Int -> [Int] -> Bool
+isMultipleOf x factors
+    | factors == [] = False
+    | head factors * last factors == x = True
+    | x `isMultipleOf` init factors = True
+    | x `isMultipleOf` tail factors = True
+    | otherwise = False
+
+factorsOf :: Int -> [Int]
+factorsOf n = [x | x <- [1..n], n `rem` x == 0]
+
+palindromicNumbersWithFactorsOfLength :: Int -> [Int]
+palindromicNumbersWithFactorsOfLength n = [x | x <- [1..((10 ^ n) ^ 2-1)], isPalindromicInt x && x `hasTwoFactorsOfLength` n]
